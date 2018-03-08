18fb9199074945b85d4c8fa572ea88de0ec14576
olivas1406 <olivas1406@msn.com>
2018-03-07 23:23:54 -0700

    buttons work, gifs load, need to pause the gifs on load/click, need to add the form to the right side to add buttons

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,50 +1,62 @@
 
 
 
-/*
-example from giphy website: 
-
-var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=YOUR_API_KEY&limit=5");
-xhr.done(function(data) { console.log("success got data", data); });
-
-giphy API key: 9OITCt8vj9Ey0OaPr8PoA9Qo3JAL9gxx
-
-*/
-
-
 $(document).ready(function(){ 
 
-    var topics = ["Rick Sanchez", "Randy Savage", "Mr T", "Farva", "Rick James", "Charlie Murphy", "Mike Tyson", "Andy Dwyer", "Fogell", "Keegan-Michael Key", "Jordan Peele", "Bill Burr"];
+    var topics = ["Super Troopers", "Superbad", "Fear and Loathing in Las Vegas", "Conan the Barbarian", "The Hangover", "Zombieland", "Hot Fuzz", "Tropic Thunder", "Black Dynamite",];
 
-    // create loop that appends a button for each string in the array   - DONE
-
-    function makeB() {                              // create the buttons
-        $(".buttonMe").empty();                     // makes sure there are no duplicates
+    function makeB() {                                                      // Create the buttons
+        $(".buttonMe").empty();                                             // Makes sure there are no duplicates
         for (var i = 0; i < topics.length; i++) {
-            var butHtml = $("<button>");            // create an HTML button
-            butHtml.addClass("greatMan");           // Add a div class called 'greatMan' to each button
-            butHtml.text(topics[i]);                // Push the string from the array to the button that was created for it
-          $(".buttonMe").append(butHtml);           // Add the buttons to the 'buttonMe' div
+            var butHtml = $("<button>");                                    // Create an HTML button
+            butHtml.addClass("greatMan");                                   // Add a div class called 'greatMan' to each button
+            butHtml.text(topics[i]);                                        // Push the string from the array to the button that was created for it
+            butHtml.attr("data-name", topics[i]);                           // Add a data attribute
+          $(".buttonMe").append(butHtml);                                   // Add the buttons to the 'buttonMe' div
         }
       }
 
-    makeB();                                        // Call the function to make the buttons
+    makeB();                                                                // Call the function to make the buttons
 
 
 
 
     // on click event, display 10 static gifs
-    // with ratings under each gif
+
+
+
+
+
+    $("button").on("click", function() {                                    // listen for click event
+        $(".aGreatMan").empty();                                            // Clear the previous Gifs
+        var greatMan = $(this).attr("data-name");                           // Pull the name of the button from the data-name attribute
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + greatMan +"&api_key=9OITCt8vj9Ey0OaPr8PoA9Qo3JAL9gxx&limit=10"; // giphy API URL
+
+        $.ajax({                                                            // AJAX request
+            url: queryURL,                                                  // Call the URL stored in queryURL
+            method: "GET"                                                   // Use 'GET'
+        })
+            .then(function(response) {                                      // Wait until the request is complete, store in 'response'
+            
+            console.log(response);    
+
+            var results = response.data;                                            // Create variable 'results' to hold resonse.data  
+                for (var  j= 0; j < results.length; j++) {                          // For Loop to loop through the 10 results
+                    var gifDiv = $("<div class='aGreatMan'>");                      // create a div class called 'aGreatMan'
+                    var rating = results[j].rating;                                 // variable to hold the rating
+                    var p = $("<p>").text("Rating: " + rating.toUpperCase());       // Push the rating to the HTML in Uppercase
+                    var personImage = $("<img>");                                   // Create an IMG HTML element
+                    personImage.attr("src", results[j].images.fixed_height.url);    // Give the IMG a source from the results
+                    gifDiv.prepend(p);                                              // Add the rating to the gif's div
+                    gifDiv.prepend(personImage);                                    // Add the IMG element and source to the gif's div
+                    $(".gifMe").prepend(gifDiv);                                    // Push to the HTML
+                }
+            })
+
+        });
+
 
     // on click event gif, gif plays, on click again gif pauses
-
-
-
-
-
-
-
-
 
 
     /////// ^ get the above working before moving on to this ^///////////
@@ -53,6 +65,15 @@ $(document).ready(function(){
     // Then make a function call that takes each topic in the array remakes the buttons on the page.
 
 });
+
+
+
+
+
+
+
+
+
 
 
 
