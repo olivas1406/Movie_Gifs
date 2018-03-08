eceb7e3631db5730862d2ed5130dc8552a77596f
olivas1406 <olivas1406@msn.com>
2018-03-07 19:32:39 -0700

    initial files

---
+++ assets/javascript/script.js
@@ -0,0 +1,26 @@
+
+
+
+/*
+example from giphy website: 
+
+var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=YOUR_API_KEY&limit=5");
+xhr.done(function(data) { console.log("success got data", data); });
+
+giphy API key: 9OITCt8vj9Ey0OaPr8PoA9Qo3JAL9gxx
+
+*/
+
+
+$(document).ready(function(){ 
+
+
+
+
+
+
+
+});
+
+
+
