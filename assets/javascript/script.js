770d55b184ddb8d44f74e21fe11618d16bb1e3fe
olivas1406 <olivas1406@msn.com>
2018-03-07 19:51:37 -0700

    initial layout, array complete

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -14,11 +14,28 @@ giphy API key: 9OITCt8vj9Ey0OaPr8PoA9Qo3JAL9gxx
 
 $(document).ready(function(){ 
 
+    var topics = ["Rick Sanchez", "Randy Savage", "Mr T", "Farva", "Rick James", "Charlie Murphy", "Mike Tyson", "Andy Dwyer", "Fogell", "Keegan-Michael Key", "Jordan Peele", "Bill Burr",];
+
+    // create loop that appends a button for each string in the array
+
+    // on click event, display 10 static gifs
+    // with ratings under each gif
+
+    // on click event gif, gif plays, on click again gif pauses
 
 
 
 
 
+
+
+
+
+
+    /////// ^ get the above working before moving on to this ^///////////
+
+    // Add a form to your page takes the value from a user input box and adds it into your topics array. 
+    // Then make a function call that takes each topic in the array remakes the buttons on the page.
 
 });
 
